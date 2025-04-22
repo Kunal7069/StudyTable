5fc0da68f8f36878216137b567f9ed0aa7b1aa3c
Kunal7069 <jaink7069@gmail.com>
2025-04-22 21:11:14 +0530

    forgot password fixed

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,15 +1,11 @@
 
-const crypto = require('crypto');
+
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const Student = require("../models/user");
 
 // Generate Admission Number
 const generateAdmissionNumber = () => "ADM" + Math.floor(100000 + Math.random() * 900000);
-
-function generateRandomPassword(length = 8) {
-  return crypto.randomBytes(length).toString('base64').slice(0, length);
-}
 
 // Token generation functions
 const generateAccessToken = (payload) => jwt.sign(payload, process.env.ACCESS_SECRET, { expiresIn: "7d" });
@@ -120,24 +116,27 @@ exports.updatePassword = async (req, res) => {
 
 exports.updatePasswordWithoutCurrentPassword = async (req, res) => {
   try {
-    const { admissionNumber } = req.body;
+    const { admissionNumber,newPassword, confirmPassword } = req.body;
 
-    if (!admissionNumber ) {
+    if (!admissionNumber || !newPassword || !confirmPassword) {
       return res.status(400).json({ message: "All fields are required" });
     }
 
-    
+    if (newPassword !== confirmPassword) {
+      return res.status(400).json({ message: "New passwords do not match" });
+    }
+
     const student = await Student.findOne({ where: { admissionNumber } });
 
     if (!student) {
       return res.status(404).json({ message: "Student not found" });
     }
 
-    const randomPassword = generateRandomPassword(); 
-    const hashedNewPassword = await bcrypt.hash(randomPassword, 10);
+    
+    const hashedNewPassword = await bcrypt.hash(newPassword, 10);
     await student.update({ password: hashedNewPassword });
 
-    res.status(200).json({ message: "Password updated successfully", new_password: randomPassword });
+    res.status(200).json({ message: "Password updated successfully" });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
@@ -240,6 +239,7 @@ exports.login = async (req, res) => {
   try {
     const { admissionNumber, password } = req.body;
     const student = await Student.findOne({ where: { admissionNumber } });
+    
     if (!student) return res.status(404).json({ message: "Student not found" });
 
     const isMatch = await bcrypt.compare(password, student.password);
